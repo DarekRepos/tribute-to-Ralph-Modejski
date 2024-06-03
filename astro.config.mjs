f013b24a385ea9cd52d83ad9f81dcc1e0a54b654
Darek <dareczek06@gmail.com>
2024-06-03 15:33:49 +0200

    Fixed navigation link URLs to match between development and Github Pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://DarekRepos.github.io',
+  site: 'https://DarekRepos.github.io/tribute-to-Ralph-Modejski/',
   base: '/tribute-to-Ralph-Modejski',
   integrations: [icon()]
 });
